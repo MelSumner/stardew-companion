f432fe2aad17a8b45d3797b4edb8f38e269cbc93
Melanie <melanie.sumner@hey.com>
2023-01-04 17:54:44 -0500

    render options

--- app/routes/activities/fishing/index.js
+++ app/routes/activities/fishing/index.js
@@ -2,9 +2,12 @@ import Route from '@ember/routing/route';
 import fetch from 'fetch';
 import { capitalize } from '@ember/string';
 
+// const API_URL = 'https://stardew-companion-api.herokuapp.com/api/fish';
+const API_URL = '/api/fish.json';
+
 export default class ActivitiesFishingIndexRoute extends Route {
   async model() {
-    let response = await fetch('/api/fish.json');
+    let response = await fetch(API_URL);
     let { data } = await response.json();
 
     // make sure the variable is declared outside of the loop
